7029acfe1641a1252db03c943272e4ea36e3618d
shuzhuang.su <shuzhuang.su@11bee.com>
2020-11-17 15:29:12 +0800

    init

--- src/main/java/com/javadoc/swaggerx/core/Swagger2Hook.java
+++ src/main/java/com/javadoc/swaggerx/core/Swagger2Hook.java
@@ -99,9 +99,6 @@ public class Swagger2Hook {
         deleteDir(parent);
     }
 
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
-    }
 
     private void checkSrc() {
         projectDir = GitUtil.cloneProject(new GitCloneParam(environment));
